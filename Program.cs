2b138880c18b4e90e6bca887b305e105e460a30a
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2023-02-16 19:42:52 +0100

    exercice

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// calculates the sum of all the numbers from n to m recursively
+namespace Program;
+
+class sumofnumbersTask{
+
+public static int sumofnumbers (int n , int m){
+
+int sum = n;
+
+if (n < m){
+    n++;
+    sum += sumofnumbers(n,m) ;
+    Console.WriteLine(sum);
+}
+return sum;
+}
+
+public static void Main (string [] args){
+Console.WriteLine (sumofnumbers(1,3));
+
+}
+
+
+}
